bcfa381289a0ff686d2f6a144c70c29c011427fd
Mark Williams <markrwilliams@gmail.com>
2015-03-22 01:17:15 -0700

    a simpler versioning strategy

--- setup.py
+++ setup.py
@@ -14,32 +14,21 @@ __author__ = 'Mark Williams'
 __contact__ = 'markrwilliams@gmail.com'
 __url__ = 'https://github.com/markrwilliams/spyce'
 __license__ = 'BSD'
-desc = ('A functional Python web framework that streamlines'
-        ' explicit development practices while eliminating'
-        ' global state.')
-
-try:
-    import vcversioner
-    __version__ = vcversioner.find_version().version
-except:
-    import traceback
-    traceback.print_exc()
-
-    __version__ = '0.0.0'
-
-
+desc = "Python CFFI bindings for FreeBSD's Capsicum sandboxing framework."
 
 if __name__ == '__main__':
     from setuptools import setup, find_packages
     from spyce._wrapper import ffi
 
     setup(name='spyce',
-          version=__version__,
           description=desc,
           long_description=__doc__,
           author=__author__,
           author_email=__contact__,
           zip_safe=False,
           packages=find_packages(),
+          setup_requires=['vcversioner'],
+          include_package_data=True,
+          vcversioner={},
           ext_package='spyce',
           ext_modules=[ffi.verifier.get_extension()])
